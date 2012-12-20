3288fe5e8b0ec9e2bf935a8312c999b2f76dac2f
vatseek <vatseek13@rambler.ru>
2012-12-20 12:11:41 +0200

    test4

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -107,7 +107,7 @@ class FeatureContext extends MinkContext
         $element = $this->getSession()->getPage()->find('css', "#content");
         $content = $element->getHtml();
 
-        var_dump($content);
+        echo($content);
 
         $pattern = '/<img src=\"http:\/\/livestreet.test\/uploads\/images(\/[0-9]{2}\/[0-9]{2}\/[0-9]{2}\/[0-9]{4}\/[0-9]{2}\/[0-9]{2}\/.*\.jpg)\">/';
         if (!preg_match_all($pattern, $content, $matches)) {
